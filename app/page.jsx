62b5d031dbb95f5d7e86030bbbf79f7694194cc3
IamRubens <contacto@iamrubens.dev>
2022-12-08 02:16:32 +0100

    add search bar

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,20 @@
 import styles from './page.module.css'
+import { BsSearch } from "react-icons/bs";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>
-        <h1>
-          Busca lo que quieras <br/> 
-          al precio que quieras
-        </h1>
+      <div className={styles.contenedor}>
+        <div className={styles.slogan}>
+          <h1>Busca lo que quieras</h1>
+          <h1>al precio que quieras</h1>
+        </div>
+        <div className={styles.search}>
+          <input type='text' placeholder='Búsqueda'/>
+          <BsSearch 
+            className={styles.search_button}
+          />
+        </div>
       </div>
     </main>
   )
