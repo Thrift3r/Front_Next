5f8a615a82353cde2291d42b3f6eee98a60e1235
IamRubens <contacto@iamrubens.dev>
2023-04-18 00:33:17 +0200

    add swr for get data

--- app/components/Product/Product.jsx
+++ app/components/Product/Product.jsx
@@ -1,9 +1,21 @@
 import styles from './product.module.css'
+import Image from 'next/image'
 
-export default function Product() {
+export default function Product({key, data}) {
+
+  console.log(data)
+
   return (
-    <div className={styles.product_box}>
-      <h2>Producto</h2>
+    <div className={styles.product_box} >
+      <p>{data.title}</p>
+      <Image 
+        src={data.images[0]}
+        alt={data.title}
+        width={150}
+        height={150}
+      />
+      <p>{data.description}</p>
+      <p>{data.price}</p>
     </div>
   )
 }
--- app/searches/[search]/page.jsx
+++ app/searches/[search]/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useSearchParams } from "next/navigation"
+import useSWR from 'swr';
 
 import Product from "../../components/Product/Product"
 import styles from "./search.module.css"
@@ -13,15 +14,30 @@ export default function SearchProducts() {
   const maxPrice = searchParams.get('maxprice')
   const minPrice = searchParams.get('minprice')
 
-  console.log(text)
-  console.log(maxPrice)
-  console.log(minPrice)
+  const { data, error } = useSWR(`http://127.0.0.1:8000/products/${text}`, async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  });
 
+  console.log(`http://127.0.0.1:8000/product/${text}`)
+  console.log(data)
+
+  console.log(error)
+
+  if (error) return <div>Failed to load data</div>;
+  if (!data) return <div>Loading...</div>;
 
   return (
     <div>
         <div className={styles.search_list}>
-          <Product />
+          {
+          data.map( e => (
+              <Product 
+                key={e.id}
+                data={e}
+              />
+            ))}
         </div>
     </div>
   )
